9ce565ad3bca5d84ad5236b5280d48f4856b5907
Lucas <l.deutschmann@rubika-edu.com>
2021-02-16 17:09:08 +0100

    playerMovement basic

---
+++ Assets/Sandbox/Lucas/Scripts/PlayerController.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+/// <summary>
+/// Lucas DEUTSCHMANN
+/// Player Controller
+/// </summary>
+[RequireComponent(typeof(PlayerInput))]
+public class PlayerController : MonoBehaviour
+{
+    public float speed;
+    bool move;
+    Vector3 direction;
+    Rigidbody rb;
+
+    private void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+    }
+    public void OnMove(InputValue value) //recupère le vecteur qui correspond à la direction donnée
+    {
+        move = !move;
+        direction = value.Get<Vector2>();
+        
+    }
+    private void Update()
+    {
+        if (move)
+        {
+            transform.position += direction * speed * Time.deltaTime; //déplace le joueur selon le vecteur et la vitesse 
+           // rb.MovePosition(transform.position + (direction * speed * Time.deltaTime));
+            //rb.AddForce(direction);
+        }
+        
+    }
+
+}
