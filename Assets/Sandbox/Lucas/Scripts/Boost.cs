c6fc714f66ea3323ef23e6e0ea46b2c55e12ccd1
Lucas D <l.deutschmann@rubika-edu.com>
2021-02-28 18:58:31 +0100

    boost Started

---
+++ Assets/Sandbox/Lucas/Scripts/Boost.cs
@@ -0,0 +1,52 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.InputSystem;
+/// <summary>
+/// Lucas DEUTSCHMANN
+/// Player Controller
+/// </summary>
+[RequireComponent(typeof(PlayerInput))]
+
+public class Boost : MonoBehaviour
+{
+    public bool clickDown;
+    public Vector2 firstPos;
+    public Vector2 secondPos;
+
+    public void OnBoost()
+    {
+        Debug.Log("onboost");
+        bool callOnce;
+        callOnce = true;
+
+        if (!clickDown && callOnce)
+        {
+            firstPos = Mouse.current.position.ReadValue();
+            callOnce = false;
+        }
+        if (clickDown && callOnce)
+        {
+            secondPos = Mouse.current.position.ReadValue();
+            callOnce = false;
+            Invoke("ResetPos", .5f);
+        }
+        clickDown = !clickDown;
+        
+    }
+    void ResetPos()
+    {
+        firstPos = Vector2.zero;
+        secondPos = Vector2.zero;
+    }
+    private void Update()
+    {
+        if (firstPos != Vector2.zero && secondPos != Vector2.zero)
+        {
+            if (secondPos.y > firstPos.y + 5)
+            {
+                Debug.Log("Booooost");
+            }
+        }
+    }
+}
