7ef506c043ec8f3f2fe45c7c3971de1241518387
Edouard Murat <e.murat@rubika-edu.com>
2021-03-04 00:57:00 +0100

    Added Terrain Manager

---
+++ Assets/Sandbox/Edouard/TerrainManager.cs
@@ -0,0 +1,24 @@
+﻿// Edouard MURAT
+
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TerrainManager : MonoBehaviour
+{
+    public List<TerrainObject> terrainPool;
+    public float terrainSize;
+    public float scrollSpeed;
+    private float distanceScroll;
+    private Queue<TerrainObject> terrainQueue;
+
+    private void Awake()
+    {
+        distanceScroll = 0f;
+    }
+
+    private void Update()
+    {
+        distanceScroll += scrollSpeed;
+    }
+}
---
+++ Assets/Sandbox/Edouard/TerrainObject.cs
@@ -0,0 +1,9 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu(fileName = "NewTerrain", menuName = "Scriptable Objects/TerrainObject", order = 1)]
+public class TerrainObject : ScriptableObject
+{
+    public GameObject terrainObject;
+}
