dcd2dfdb9099803c2f147e2f9ac6d8df54ee36a9
Lucas D <l.deutschmann@rubika-edu.com>
2021-03-06 13:56:57 +0100

    boost works

--- Assets/Scripts/Manager/TerrainManager.cs
+++ Assets/Scripts/Manager/TerrainManager.cs
@@ -9,6 +9,8 @@ public class TerrainManager : MonoBehaviour
     public List<TerrainObject> terrainPool;
     public float terrainLenght;
     public float scrollSpeed;
+    public float boostSpeed;
+    float baseScrollspeed;
     public int terrainCount;
     private Queue<GameObject> terrainQueue;
     private GameObject worldParentObject;
@@ -21,7 +23,7 @@ public class TerrainManager : MonoBehaviour
     private void Start()
     {
         worldParentObject = GameObject.Find("----- WORLD -----");
-
+        baseScrollspeed = scrollSpeed;
         for(int i = 0; i<terrainCount; i++)
         {
             AddTerrain(new Vector3(0, 0, 200*i));
@@ -56,11 +58,19 @@ public class TerrainManager : MonoBehaviour
         }
     }
 
-    public void Boost()
+    public void Boost(float duration)
     {
         // TO-DO
         Debug.Log("Boost!");
+        scrollSpeed = boostSpeed;
+        Invoke("EndBoost", duration);
     }
+    
+    void EndBoost()
+    {
+        scrollSpeed = baseScrollspeed;
+    }
+
 
     private void AddTerrain(Vector3 position)
     {
