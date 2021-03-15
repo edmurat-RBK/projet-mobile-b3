7117b547ef0ebc5e529addc7c9c3b31c8ff1d987
Matis <m.duperray@rubika-edu.com>
2021-03-15 16:51:18 +0100

    Dummy are smart

--- Assets/Scripts/Ennemi/GlobalEnnemiBehavior.cs
+++ Assets/Scripts/Ennemi/GlobalEnnemiBehavior.cs
@@ -39,8 +39,17 @@ public class GlobalEnnemiBehavior : MonoBehaviour
 
     public void CheckDirection()
     {
-        if(Physics.Raycast(transform.position + Vector3.up * 50f, transform.forward, out hit, 4f, obstacleMask)) //Devant
+        #region Debug
+        Debug.DrawRay(transform.position, transform.forward * 50f, Color.red);
+        Debug.DrawRay(transform.position, (transform.forward + transform.right / 3) * 20, Color.red);
+        Debug.DrawRay(transform.position, (transform.forward - transform.right / 3) * 20, Color.red);
+        Debug.DrawRay(transform.position + Vector3.up, transform.right * 20, Color.blue);
+        Debug.DrawRay(transform.position + Vector3.up, -transform.right * 20, Color.blue);
+        #endregion
+
+        if (Physics.Raycast(transform.position, transform.forward, out hit, 50f, obstacleMask)) //Devant
         {
+            Debug.Log("MOVE sale chien");
             obstacleAhead = true;
         }
         else
@@ -48,7 +57,7 @@ public class GlobalEnnemiBehavior : MonoBehaviour
             obstacleAhead = false;
         }
 
-        if(Physics.Raycast(transform.position + Vector3.up * 50f, transform.forward + transform.right, out hit, 2f, obstacleMask)) //Diagonal Droite
+        if(Physics.Raycast(transform.position, (transform.forward + transform.right)/3, out hit, 20f, obstacleMask)) //Diagonal Droite
         {
             obstacleOnRight = true;
         }
@@ -57,7 +66,7 @@ public class GlobalEnnemiBehavior : MonoBehaviour
             obstacleOnRight = false;
         }
 
-        if (Physics.Raycast(transform.position + Vector3.up * 50f, transform.forward - transform.right, out hit, 2f, obstacleMask)) //Diagonal Gauche
+        if (Physics.Raycast(transform.position, (transform.forward - transform.right)/3, out hit, 20f, obstacleMask)) //Diagonal Gauche
         {
             obstacleOnLeft = true;
         }
@@ -89,20 +98,30 @@ public class GlobalEnnemiBehavior : MonoBehaviour
     }
     public void Movement()
     {
-        if(obstacleAhead == true)
+        if(obstacleAhead == true)   //L'ennemi fait face à un obstacle
         {
-            if(!obstacleOnRight && !obstacleOnLeft)
+            if(!obstacleOnRight && !obstacleOnLeft) //La voie est dégagée des deux côtés
             {
-                transform.position = Vector3.MoveTowards(transform.position, transform.position + (Vector3.right * 100), 100f*Time.deltaTime);
+                transform.position = Vector3.MoveTowards(transform.position, transform.position + (Vector3.right), 10f*Time.deltaTime);
             }
-            else if(obstacleOnLeft)
+            else if(obstacleOnLeft && !obstacleOnRight) //La voie est dégagée sur la droite, mais pas sur la gauche
             {
-                transform.position = Vector3.MoveTowards(transform.position, transform.position + (Vector3.right * 100), 100f * Time.deltaTime);
+                transform.position = Vector3.MoveTowards(transform.position, transform.position + (Vector3.right), 10f * Time.deltaTime);   //Mouvement droite
             }
-            else if(obstacleOnRight)
+            else if(obstacleOnRight && !obstacleOnLeft) //La voie est dégagée sur la gauche, mais pas sur la droite
             {
-                transform.position = Vector3.MoveTowards(transform.position, transform.position + (Vector3.left * 100), 100f * Time.deltaTime);
+                transform.position = Vector3.MoveTowards(transform.position, transform.position + (Vector3.left), 10f * Time.deltaTime);    //Mouvement gauche
             }
+        }
+        
+        else if(obstacleOnRight && !obstacleOnLeft)
+        {
+            transform.position = Vector3.MoveTowards(transform.position, transform.position + (Vector3.left), 10f * Time.deltaTime);
+        }
+
+        else if(obstacleOnLeft && !obstacleOnRight)
+        {
+            transform.position = Vector3.MoveTowards(transform.position, transform.position + (Vector3.right), 10f * Time.deltaTime);
         }
     }
 }
