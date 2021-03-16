8214a001d16e1705676d8e869457009893352898
Edouard Murat <e.murat@rubika-edu.com>
2021-03-16 13:42:52 +0100

    Cleaned up project folders

---
+++ Assets/Scripts/Economic/MarianneCoin.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MarianneCoin : MonoBehaviour
+{
+     void OnTriggerEnter(Collider other)
+    {
+        if (other.tag == "Player")
+        {
+            TotalPiece.tpinstance.totalPiece += 1;
+            Destroy(gameObject);
+            
+        }
+    }
+}
---
+++ Assets/Scripts/Enemy/DummyBehavior.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DummyBehavior : GlobalEnnemiBehavior
+{
+
+
+
+
+
+    private void Start()
+    {
+        GameManager.Instance.ennemiManager.ennemiList.Add(this.gameObject);
+    }
+    private void Update()
+    {
+        CheckDirection();
+        Movement();
+        MoveBack(false);
+
+
+        if(ennemiLife <= 0 || transform.position.z < GameManager.Instance.ennemiManager.deadZone.position.z)
+        {
+            Death();
+        }
+    }
+}
