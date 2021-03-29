00c8252b7df420b511f89e23d6891926516e6d9c
Lucas <l.deutschmann@rubika-edu.com>
2021-03-29 14:51:55 +0200

    life display

---
+++ Assets/Sandbox/Lucas/Scripts/LifeDisplay.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class LifeDisplay : MonoBehaviour
+{
+    public Text playerLife;
+    int playerHp;
+    // Start is called before the first frame update
+    void Start()
+    {
+        playerHp = (int)GameManager.Instance.playerManager.playerLife;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        playerLife.text = playerHp.ToString();
+    }
+}
