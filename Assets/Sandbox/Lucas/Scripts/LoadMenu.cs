50b732776ea03eae0d3fbca40aecd39886f6d7d0
Lucas <l.deutschmann@rubika-edu.com>
2021-03-30 17:01:58 +0200

    Ca avance

---
+++ Assets/Sandbox/Lucas/Scripts/LoadMenu.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class LoadMenu : MonoBehaviour
+{
+    public Text highscoreText;
+    public Text coinsText;
+    int highscore;
+    int coins;
+    // Start is called before the first frame update
+    void Start()
+    {
+        LoadValues();
+    }
+
+    // Update is called once per frame
+    void LoadValues()
+    {
+        Datas data = DataManager.DMInstance.Load();
+
+        if (data != null)
+        {
+            coins = data.coinsCollected;
+            highscore = data.highscore;
+        }
+        DisplayValues();
+    }
+
+    void DisplayValues()
+    {
+        highscoreText.text = "Hiscore: " + highscore.ToString();
+        coinsText.text = "Coins: " + coins.ToString();
+    }
+}
