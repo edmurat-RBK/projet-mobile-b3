8a53e225f725a7a1687bbff3189a027f2598d0a7
Lucas D <l.deutschmann@rubika-edu.com>
2021-04-08 20:35:54 +0200

    check si la quest est finie

--- Assets/Sandbox/Lucas/Scripts/QuestManager.cs
+++ Assets/Sandbox/Lucas/Scripts/QuestManager.cs
@@ -17,19 +17,59 @@ public class QuestManager : MonoBehaviour
     
     List<QuestsType> quests = new List<QuestsType>();
     
-    public int enemiesDestroyed;
-    public int coinsPickedUp;
-    public int obstaclesDestroyed;
-    public int totalScore;
+    int enemiesDestroyed;
+    public int enemiesDestroyedGoal;
+    int coinsPickedUp;
+    public int coinsPickedUpGoal;
+    int obstaclesDestroyed;
+    public int obstaclesDestroyedGoal;
+    int totalScore;
+    public int totalScoreGoal;
 
     public int[] currentQuests;
 
     private void Start() 
     {
-        SelectNextQuest();
+        InitQuests();
     }
 
-    void SelectNextQuest()
+    private void Update() 
+    {
+        if (enemiesDestroyed == enemiesDestroyedGoal)
+        {
+            if (quests.Contains(QuestsType.DestroyEnemies))
+            {
+                quests.Remove(QuestsType.DestroyEnemies);
+                NextQuest(QuestsType.DestroyEnemies);
+            }
+        }
+        else if(coinsPickedUp == coinsPickedUpGoal)
+        {
+            if (quests.Contains(QuestsType.PickupCoins))
+            {
+                quests.Remove(QuestsType.PickupCoins);
+                NextQuest(QuestsType.PickupCoins);
+            }
+        }
+        else if (obstaclesDestroyed == obstaclesDestroyedGoal)
+        {
+            if (quests.Contains(QuestsType.DestroyObstacles))
+            {
+                quests.Remove(QuestsType.DestroyObstacles);
+                NextQuest(QuestsType.DestroyObstacles);
+            }
+        }
+        else if (totalScore == totalScoreGoal)
+        {
+            if (quests.Contains(QuestsType.GetScore))
+            {
+                quests.Remove(QuestsType.GetScore);
+                NextQuest(QuestsType.GetScore);
+            }
+        }
+    }
+
+    void InitQuests()
     {
         int enumCount = Enum.GetNames(typeof(QuestsType)).Length;
         for (int i = 0; i < numberOfQuests; i++)
@@ -45,6 +85,22 @@ public class QuestManager : MonoBehaviour
                 quests.Add((QuestsType)rnd);
                 Debug.Log(quests[i]);
             }
+        }
+    }
+
+    void NextQuest(QuestsType questsType)
+    {
+        int enumCount = Enum.GetNames(typeof(QuestsType)).Length;
+        int rnd = UnityEngine.Random.Range(0,enumCount);
+            
+        if (quests.Contains((QuestsType)rnd) || quests.Contains(questsType))
+        {
+            NextQuest(questsType);
+        }
+        else
+        {
+            quests.Add((QuestsType)rnd);
+                
         }
     }
 }
