9fb93d11f234cf3c450cfdde6ac8ac351146edfb
Lucas D <l.deutschmann@rubika-edu.com>
2021-04-10 18:24:02 +0200

    weapons for the shop

--- Assets/Sandbox/Lucas/Scripts/Weapon.cs
+++ Assets/Sandbox/Lucas/Scripts/Weapon.cs
@@ -17,7 +17,9 @@ public class Weapon : MonoBehaviour
 
 
     public LayerMask ennemis;
-    public float range;
+    public float rangeMoissoneuse;
+    public float rangeCopilote;
+    public float rangeLanceFlammes;
     RaycastHit hit;
 
     // Start is called before the first frame update
@@ -32,12 +34,80 @@ public class Weapon : MonoBehaviour
     {
         if (myWeapon == WeaponType.Moissoneuse)
         {
-            Debug.DrawRay(transform.position, transform.forward * range, Color.red);
-        if (Physics.Raycast(transform.position,transform.forward,out hit, range,ennemis))
-        {
-            hit.transform.GetComponent<GlobalEnnemiBehavior>().Death();
+        
         }
+
+        switch (myWeapon)
+        {
+            case WeaponType.Moissoneuse:
+                Moissoneuse();
+                break;
+            case WeaponType.Copilote:
+                Copilote();
+                break;
+            case WeaponType.MiniGrue:
+                Grue();
+                break;
+            case WeaponType.LanceFlames:
+                LanceFlammes();
+                break;    
+            case WeaponType.Enceintes:
+                Enceintes();
+                break;
+            default:
+                break;
         }
         
+    }
+
+    void Moissoneuse()
+    {
+        Debug.DrawRay(transform.position, transform.forward * rangeMoissoneuse, Color.red);
+                if (Physics.Raycast(transform.position,transform.forward,out hit, rangeMoissoneuse,ennemis))
+                {
+                    hit.transform.GetComponent<GlobalEnnemiBehavior>().Death();
+                }
+    }
+
+    void Copilote()
+    {
+        Debug.DrawRay(transform.position, (transform.right + transform.forward) * rangeCopilote, Color.red);
+                Debug.DrawRay(transform.position, (-transform.right + transform.forward) * rangeCopilote, Color.red);
+                if (Physics.Raycast(transform.position,transform.right + transform.forward,out hit, rangeCopilote,ennemis))
+                {
+                    hit.transform.GetComponent<GlobalEnnemiBehavior>().Death();
+                    Debug.Log("Tue ennemi de droite avec le copilote");
+                }
+                if (Physics.Raycast(transform.position,-transform.right + transform.forward,out hit, rangeCopilote,ennemis))
+                {
+                    hit.transform.GetComponent<GlobalEnnemiBehavior>().Death();
+                    Debug.Log("Tue ennemi de gauche avec le copilote");
+                }
+    }
+
+    void LanceFlammes()
+    {
+        Debug.DrawRay(transform.position, transform.right * rangeLanceFlammes, Color.red);
+                Debug.DrawRay(transform.position, -transform.right * rangeLanceFlammes, Color.red);
+                if (Physics.Raycast(transform.position,transform.right,out hit, rangeLanceFlammes,ennemis))
+                {
+                    hit.transform.GetComponent<GlobalEnnemiBehavior>().Death();
+                    Debug.Log("Tue ennemi de droite avec le lance flammes");
+                }
+                if (Physics.Raycast(transform.position,-transform.right,out hit, rangeLanceFlammes,ennemis))
+                {
+                    hit.transform.GetComponent<GlobalEnnemiBehavior>().Death();
+                    Debug.Log("Tue ennemi de gauche avec le lance flammes");
+                }
+    }
+
+    void Grue()
+    {
+
+    }
+
+    void Enceintes()
+    {
+
     }
 }
