001f6196cc06725ca5041a6bbe91c489c3aed4c9
Matis <m.duperray@rubika-edu.com>
2021-04-13 13:02:01 +0200

    RunThemeAdded

---
+++ Assets/Scripts/Manager/AudioManager.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AudioManager : MonoBehaviour
+{
+    [Header("Musics")]
+    [Space(10)]
+
+    public AK.Wwise.Event runMusic;
+
+
+
+
+
+    private void Start()
+    {
+        runMusic.Post(gameObject);
+    }
+}
