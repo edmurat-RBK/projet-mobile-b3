011890fd2ebc2b97023ecba4b089d9ec4b0ddc7f
Marianne <m.cetkovic@rubika-edu.com>
2021-04-15 17:48:36 +0200

    Minor fix

---
+++ Assets/fxMovement.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class fxMovement : MonoBehaviour
+{
+void Update()
+    {
+        transform.position = Vector3.MoveTowards(transform.position, transform.position + (Vector3.back * 100), GameManager.Instance.terrainManager.scrollSpeed/1.5f * Time.deltaTime);
+    }
+}
