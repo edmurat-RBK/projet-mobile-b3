7e6744c7be3648796c0cb4c53252a33549c8d5e3
Lucas <l.deutschmann@rubika-edu.com>
2021-05-11 12:43:38 +0200

    fix cam

---
+++ Assets/Scripts/Manager/CamManager.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CamManager : MonoBehaviour
+{
+    public Transform target;
+    public Vector3 offset;
+    // Start is called before the first frame update
+    
+
+    // Update is called once per frame
+    void LateUpdate()
+    {
+        transform.position = target.position + offset;
+    }
+}
