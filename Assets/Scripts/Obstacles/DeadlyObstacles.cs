6c494a65f6cdce7f9252ffc595a48f652276825a
Marianne <m.cetkovic@rubika-edu.com>
2021-05-25 23:46:54 +0200

    Add new LD one mesh sans les colliders mortels + New economic system

---
+++ Assets/Scripts/Obstacles/DeadlyObstacles.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DeadlyObstacles : MonoBehaviour
+{
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.tag == "Player")
+        {
+            if (GameManager.Instance.playerManager.shield <= 0)
+            {
+                GameManager.Instance.playerManager.playerLife -= GameManager.Instance.playerManager.playerLife;
+            }
+            else
+            {
+                GameManager.Instance.playerManager.shield -= GameManager.Instance.playerManager.shield;
+            }
+        }
+    }
+}
