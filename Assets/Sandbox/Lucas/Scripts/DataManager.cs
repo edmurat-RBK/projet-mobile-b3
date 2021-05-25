e6920796d3bb065de6ca896731d7e184b4fe2e0f
Lucas D <l.deutschmann@rubika-edu.com>
2021-05-25 12:19:32 +0200

    fixed :)))

--- Assets/Sandbox/Lucas/Scripts/DataManager.cs
+++ Assets/Sandbox/Lucas/Scripts/DataManager.cs
@@ -49,7 +49,7 @@ public class DataManager : MonoBehaviour
     {
         StreamWriter streamWriter = new StreamWriter(path2, false);
         ShopData data = new ShopData { scoreMulti = scoreMulti,coinsMulti = coinsMulti,extraLives=extraLives,maxLife=maxLife};
-
+        Debug.Log("hoho");
         serializer2.Serialize(streamWriter, data);
         streamWriter.Close();
         SceneManager.LoadScene(index);
