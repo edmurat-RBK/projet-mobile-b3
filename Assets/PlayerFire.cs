5d17dc807681816191cc0850c44c464fbf48044e
Lucas D <l.deutschmann@rubika-edu.com>
2021-05-30 16:43:09 +0200

    fire done

--- Assets/PlayerFire.cs
+++ Assets/PlayerFire.cs
@@ -4,9 +4,14 @@ using UnityEngine;
 
 public class PlayerFire : MonoBehaviour
 {
-    void OnTriggerEnter(Collider other)
+
+    public LayerMask fireLayer;
+    RaycastHit raycastHit;
+    bool canTakeDamage = true;
+
+    private void Update()
     {
-        if (other.tag == "Fire")
+        if (Physics.Raycast(transform.position, transform.position + Vector3.forward * 5, out raycastHit, 20f, fireLayer) && canTakeDamage)
         {
             if (GameManager.Instance.playerManager.shield <= 0)
             {
@@ -17,7 +22,32 @@ public class PlayerFire : MonoBehaviour
             {
                 GameManager.Instance.playerManager.shield -= 1;
             }
-
+            canTakeDamage = false;
+            StartCoroutine(waitForDamage());
+            Debug.Log("okidoki");
         }
     }
+
+    IEnumerator waitForDamage()
+    {
+        yield return new WaitForSeconds(.3f);
+        canTakeDamage = true;
+    }
+    // void OnTriggerStay(Collider other)
+    // {
+    //     if (other.tag == "Fire")
+    //     {
+    //         if (GameManager.Instance.playerManager.shield <= 0)
+    //         {
+    //             GameManager.Instance.playerManager.playerLife -= 1;
+
+    //         }
+    //         else
+    //         {
+    //             GameManager.Instance.playerManager.shield -= 1;
+    //         }
+
+    //     }
+    // }
+
 }
