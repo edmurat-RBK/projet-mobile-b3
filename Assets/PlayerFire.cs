957f92a4639dba0ebb9953f39b923a8994bbc5ae
Marianne <m.cetkovic@rubika-edu.com>
2021-05-30 13:03:04 +0200

    Minors Fixs & adds (images shops, fix ld, ajout script sur player Fire damage)

---
+++ Assets/PlayerFire.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerFire : MonoBehaviour
+{
+    void OnTriggerEnter(Collider other)
+    {
+        if (other.tag == "Fire")
+        {
+            if (GameManager.Instance.playerManager.shield <= 0)
+            {
+                GameManager.Instance.playerManager.playerLife -= 1;
+
+            }
+            else
+            {
+                GameManager.Instance.playerManager.shield -= 1;
+            }
+
+        }
+    }
+}
